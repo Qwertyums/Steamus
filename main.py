39b16f3eacf8fdc15b924f0727dfaff7d11b0021
Qwertyums <143101135+Qwertyums@users.noreply.github.com>
2024-05-31 01:29:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,12 @@
-#Небольшая заготовка(бота можно найти по этому тегу: @Stimus_bot)
-import asyncio
-import logging
-from DATA import BOT_TOKEN
+from data_base.DATA import BOT_TOKEN
 from aiogram import Bot, Dispatcher, F
-from aiogram.filters import CommandStart
-from aiogram.types import (KeyboardButton, Message, ReplyKeyboardMarkup,ReplyKeyboardRemove)
-
+from handlers import user_handlers
 
 bot_token = BOT_TOKEN
 bot = Bot(token=bot_token)
 dp = Dispatcher()
 
-button_1 = KeyboardButton(text='Цены')
-button_2 = KeyboardButton(text='Портфели')
-
-keyboard = ReplyKeyboardMarkup(keyboard=[[button_1, button_2]])
-
-@dp.message(CommandStart())
-async def process_start_command(message: Message):
-    await message.answer(
-        text='Куда топаем?',
-        reply_markup=keyboard
-    )
-
-keyboard = ReplyKeyboardMarkup(
-    keyboard=[[button_1, button_2]],
-    resize_keyboard=True,
-    one_time_keyboard=True
-)
-
-@dp.message(F.text == 'Цены')
-async def process_dog_answer(message: Message):
-    await message.answer(
-        text='Работаю над реализацией '
-    )
-
-
-@dp.message(F.text == 'Портфели')
-async def process_cucumber_answer(message: Message):
-    await message.answer(
-        text='Работаю над реализацией '
-    )
-
+dp.include_router(user_handlers.router)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
